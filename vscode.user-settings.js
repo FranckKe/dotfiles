68d0d84e0bd36749710aa2673ec098f85c02f154
Franck Kerbiriou <franck.kerbiriou@gmail.com>
2018-10-05 23:16:27 +0200

    Add vs user settings

---
+++ vscode.user-settings.js
@@ -0,0 +1,27 @@
+ {
+  "window.zoomLevel": 1,
+  "editor.fontFamily": "'DejaVu Sans Mono'",
+  "workbench.iconTheme": "material-icon-theme",
+  "workbench.colorTheme": "Nord",
+  "files.watcherExclude": {
+    "**/.git/objects/**": true,
+    "**/.git/subtree-cache/**": true,
+    "**/node_modules/**": true,
+    "**/npm-packages-offline-cache/**": true,
+    "**/janusGraph/**": true,
+    "**/public/lib/**": true
+  },
+  // "cSpell.language": ",fr",
+  "editor.formatOnSave": true,
+  "[ejs]": {
+    "editor.insertSpaces": false
+  },
+  "editor.wordWrap": "on",
+  "editor.tokenColorCustomizations": {
+    "comments": "#999999"
+  },
+  "editor.scrollBeyondLastLine": false,
+  "editor.formatOnPaste": true,
+  "editor.formatOnType": false,
+  "files.trimTrailingWhitespace": true
+}
